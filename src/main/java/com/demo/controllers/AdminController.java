8daf92cb3922c471c6c5b4361f5689e2519d3525
Huy <97960715+contimden@users.noreply.github.com>
2024-04-26 19:20:19 +0700

    mm

--- src/main/java/com/demo/controllers/AdminController.java
+++ src/main/java/com/demo/controllers/AdminController.java
@@ -1,22 +1,19 @@
 package com.demo.controllers;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.demo.services.AccountJPAService;
-
 @Controller
 @RequestMapping("admin")
 public class AdminController {
-
 	@Autowired
 	private AccountJPAService accountJPAService;
 
 	@GetMapping("index")
-	public String admin() {
+	public String admin(ModelMap modelMap) {
+		modelMap.put("accounts", accountJPAService.findAll());
 		return "admin/index";
 	}
 
