9f9cf42de9b3b7d72e083bca24c1f7add13d9c3b
Huy <97960715+contimden@users.noreply.github.com>
2024-04-26 19:04:36 +0700

    Revert "commit news changes"
    This reverts commit b45b0f1e75896c257c174ec8d65178ebfc7d0db0.

--- src/main/java/com/demo/controllers/AdminController.java
+++ src/main/java/com/demo/controllers/AdminController.java
@@ -1,26 +1,28 @@
 package com.demo.controllers;
 
-import com.demo.services.AccountJPAService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.demo.services.AccountJPAService;
 
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
-    @Autowired
-    private AccountJPAService accountJPAService;
+	@Autowired
+	private AccountJPAService accountJPAService;
 
-    @GetMapping("index")
-    public String admin() {
-        return "admin/index";
-    }
+	@GetMapping("index")
+	public String admin() {
+		return "admin/index";
+	}
 
-    @GetMapping("music")
-    public String music() {
-        return "admin/music";
-    }
+	@GetMapping("music")
+	public String music() {
+		return "admin/music";
+	}
 
 }
