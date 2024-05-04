367b1cb13460bc84bca1475585244b2f0e22617c
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-05-04 17:39:29 +0700

    delete accountsongrepo

--- src/main/java/com/demo/repositories/AccountSongRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.entities.AccountSong;
-
-public interface AccountSongRepository extends JpaRepository<AccountSong, Integer> {
-
-}
