54657ac246eaec680f9c6d7876acb16c85069eaf
ToanNguyen9602 <toannguyen9602@gmail.com>
2024-05-04 11:09:40 +0700

    commit sualoiSong_Account

--- src/main/java/com/demo/repositories/SongRepository.java
+++ src/main/java/com/demo/repositories/SongRepository.java
@@ -9,10 +9,8 @@ import org.springframework.data.repository.query.Param;
 import com.demo.entities.Song;
 
 public interface SongRepository extends JpaRepository<Song, Integer> {
-
     @Query("from Song where author.authorId = :authorId")
     public List<Song> findByAuthorId(@Param("authorId") int authorId);
-
     @Query("SELECT s FROM Song s JOIN s.singers sg WHERE sg.singerId = :singerId")
     List<Song> findSongsBySingerId(@Param("singerId") int singerId);
 }
