95a8edc3d731299f1c36121ad73fe01c4bf2bf4c
Huy <97960715+contimden@users.noreply.github.com>
2024-05-15 13:37:33 +0700

    ui album find detail

--- src/main/java/com/demo/controllers/SongDetailController.java
+++ src/main/java/com/demo/controllers/SongDetailController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -205,5 +206,9 @@ public class SongDetailController {
         }
     }
 
-
+    @GetMapping("musicDetail")
+	public String SongDetail() {
+		
+		return "user/musicDetail/find";
+	}
 }
